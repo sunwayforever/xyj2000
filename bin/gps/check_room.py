3309f134d1d3676b0a885373ab2dc3f590def3a8
wei.sun <wei.sun@spreadtrum.com>
2017-08-22 12:50:56 +0800

    nil

--- bin/gps/check_room.py
+++ bin/gps/check_room.py
@@ -10,25 +10,28 @@ from ..common import logger
 
 def check_room(conn, zone, room, desc, exits):
     room = fixup_room(room)
-    # sql = "select roomno from mud_room where roomname = '%s'" % (room)
-    # rows = conn.execute(sql).fetchall();
-    # if len(rows) == 1:
-    #     return rows[0][0]
+    sql = "select roomno,abbr from mud_room where roomname = '%s'" % (room)
+    rows = conn.execute(sql).fetchall()
+    if len(rows) == 1:
+        return rows[0][0], rows[0][1]
 
-    # sql = "select roomno from mud_room where roomname = '%s' and zone = '%s'" % (room, zone)
-    # rows = conn.execute(sql).fetchall();
-    # if len(rows) == 1:
-    #     return rows[0][0]
+    sql = "select roomno,abbr from mud_room where roomname = '%s' and zone = '%s'" % (
+        room, zone)
+    rows = conn.execute(sql).fetchall()
+    if len(rows) == 1:
+        return rows[0][0], rows[0][1]
 
-    # sql = "select roomno from mud_room where roomname = '%s' and description = '%s'" % (room, desc)
-    # rows = conn.execute(sql).fetchall();
-    # if len(rows) == 1:
-    #     return rows[0][0]
+    sql = "select roomno,abbr from mud_room where roomname = '%s' and description = '%s'" % (
+        room, desc)
+    rows = conn.execute(sql).fetchall()
+    if len(rows) == 1:
+        return rows[0][0], rows[0][1]
 
-    # sql = "select roomno from mud_room where roomname = '%s' and description = '%s' and exits = '%s'" % (room, desc, exits)
-    # rows = conn.execute(sql).fetchall();
-    # if len(rows) == 1:
-    #     return rows[0][0]
+    sql = "select roomno,abbr from mud_room where roomname = '%s' and description = '%s' and exits = '%s'" % (
+        room, desc, exits)
+    rows = conn.execute(sql).fetchall()
+    if len(rows) == 1:
+        return rows[0][0], rows[0][1]
 
     sql = "select max(roomno),abbr from mud_room where zone = '%s' and roomname = '%s' and description = '%s' and exits = '%s'" % (
         zone, room, desc, exits)
